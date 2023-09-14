46d67aeac22c7774313ed3f1f27e4d8626e0d865
Aniket <aniketsingh1412@gmail.com>
2023-09-14 13:23:40 +0530

    uff gain

--- Helloworld.java
+++ Helloworld.java
@@ -1,9 +1,9 @@
-public class HelloWorld {
+public class Helloworld {
 
 	public static void main(String[] args) 
 	{
 		int a=10,b=5;
-		System.out.println(a/b);
+		System.out.println(a+b+66);
 		
 	}
 
